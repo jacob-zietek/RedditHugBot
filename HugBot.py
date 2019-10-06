470fb1fa69d1aa19fccde3ac72fdb13d94f32f0d
jacob-zietek <xoqcpn@gmail.com>
2019-10-05 21:49:06 -0400

    Added subreddits, regex processing, and extensible reddit support.

--- HugBot.py
+++ HugBot.py
@@ -1,10 +1,58 @@
 import praw
+import time
+import re
+
+reddit = praw.Reddit('bot1')
+user = reddit.redditor('the-hug-bot')
+
+regexMatch = '.*need.*hug'
 
 mentalHealthSubreddits = [
-mentalHealth,
-depression,
-tifu,
-raisedbynarcissists,
-survivorsofabuse,
-
+"mentalHealth",
+"tifu",
+"raisedbynarcissists",
+"act",
+"sat",
+"applyingtocollege",
+"teenagers",
+"insaneparents",
+"apstudents",
+"offmychest"
 ]
+
+'''
+# Testing environment
+mentalHealthSubreddits = [
+"7iooibottesting"
+]
+'''
+
+
+global commentList
+commentList = []
+
+def processComment(comment):
+    global commentList
+    commentID = comment.id
+    if commentID not in commentList:
+        bodyText = comment.body
+        if re.search(regexMatch, bodyText, re.IGNORECASE):
+            print("New comment posted.\n", flush=True)
+            commentList.append(commentID)
+            comment.reply("*Hug*")
+
+while True:
+    for sub in mentalHealthSubreddits:
+        #print(commentList, flush=True)
+        #print("Reading new subreddit...\n" + sub, flush=True)
+        for comment in reddit.subreddit(sub).stream.comments(pause_after=0):
+            time.sleep(0.01)
+            if comment is None:
+                break
+            else:
+                try:
+                    processComment(comment)
+                except:
+                    print("Comment processing produced an error. Trying to wait to resolve problem.", flush=True) #New accounts are rate limited
+                    time.sleep(60)
+                    processComment(comment)
